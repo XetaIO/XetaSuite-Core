cfa52dd62e21d7be4fe054fb85f304dea78acdd2
Xety <emeric@xetaravel.com>
2025-11-18 22:54:41 +0100

    Setup sanctum/fortify routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+ |--------------------------------------------------------------------------
+ | API Routes
+ |--------------------------------------------------------------------------
+ */
+Route::group(['prefix'    => 'v1', 'middleware' => 'auth:sanctum'], function() {
+
+        // Get authenticated user
+        Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+            return new UserResource($request->user());
+        });
+
+        // Incidents
+        //Route::middleware('auth:sanctum')->apiResource('incidents', \App\Http\Controllers\Api\V1\IncidentController::class);
+
+        // ...
+
+});
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
+use Laravel\Fortify\Http\Controllers\PasswordResetLinkController;
+use Laravel\Fortify\Http\Controllers\NewPasswordController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('api/v1')->middleware(['web'])->group(function () {
+
+    Route::prefix('auth')->group(function () {
+        // Auth
+        Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+
+        // Register
+        Route::post('/register', [RegisteredUserController::class, 'store']);
+
+        // Password reset
+        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
+        Route::post('/reset-password', [NewPasswordController::class, 'store']);
+    });
 });
