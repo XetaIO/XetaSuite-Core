61f93d4b6a1d3124e0a81351ee13ba56b3cd6350
Xety <zoro.fmt@gmail.com>
2025-11-19 17:55:07 +0100

    Continue to setup api

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'    => 'v1', 'middleware' => 'auth:sanctum'], function() {
 
         // Get authenticated user
-        Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        Route::middleware('auth:sanctum')->get('/auth/user', function (Request $request) {
             return new UserResource($request->user());
         });
 
